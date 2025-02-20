3edb270c916c155a4f9d9277624e8f50757bbd54
JakubGrezl <jakub.grezl@outlook.cz>
2025-02-20 15:43:51 +0100

    description in other file than main.py was even a bad idea...

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from app.include_routers import include_routers
 from pathlib import Path
 import os
 
-description = Path(os.path.dirname(os.path.abspath(__file__)) + "/description.md").read_text(encoding="utf-8")
+description = """
+    * description
+"""
 
 app = FastAPI(
     title="Plant Alert API",
